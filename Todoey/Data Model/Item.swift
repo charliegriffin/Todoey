6ef3045dbbff88c23bc485852591448d4528b852
Charlie Griffin <charliegriffin@email.arizona.edu>
2018-03-10 16:38:30 -0600

    Conver to MVC design pattern.

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Charles Griffin on 3/10/18.
+//  Copyright © 2018 Charles Griffin. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
