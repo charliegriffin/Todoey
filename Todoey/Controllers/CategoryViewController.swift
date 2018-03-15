946fd092701a029be194bbcb9d702f07bc7b507f
Charlie Griffin <charliegriffin@email.arizona.edu>
2018-03-15 07:46:21 -0500

    add categories view. commit before challenge of setting up the tableview methods

---
+++ Todoey/Controllers/CategoryViewController.swift
@@ -0,0 +1,35 @@
+//
+//  CategoryViewController.swift
+//  Todoey
+//
+//  Created by Charles Griffin on 3/15/18.
+//  Copyright © 2018 Charles Griffin. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class CategoryViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    //MARK: - TableView Datasource Methods
+    
+    //MARK: - Data Manipulation Methods
+    
+    //MARK: - Add New Categories
+
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+    }
+    
+    
+    
+    //MARK: - TableView Delegate Methods
+    
+
+    
+}
