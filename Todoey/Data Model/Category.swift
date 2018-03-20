8f1b77c33fc6189402c3c4eb73152cbc88373718
Charlie Griffin <charliegriffin@email.arizona.edu>
2018-03-20 08:30:59 -0500

    implement CRUD using RealmSwift

---
+++ Todoey/Data Model/Category.swift
@@ -0,0 +1,15 @@
+//
+//  Category.swift
+//  Todoey
+//
+//  Created by Charles Griffin on 3/20/18.
+//  Copyright © 2018 Charles Griffin. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class Category: Object {
+    @objc dynamic var name : String = ""
+    let items = List<Item>()
+}
